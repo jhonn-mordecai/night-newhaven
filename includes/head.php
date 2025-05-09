50e59d45b2fc12d414736df956a5ee9a144e3c38
John Mordecai <john.mordecai@gmail.com>
2025-05-09 15:19:04 -0400

    Updated favicon and meta descriptions

--- includes/head.php
+++ includes/head.php
@@ -5,19 +5,19 @@
 		<!-- Required meta tags -->
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		<meta name="description" content="NIGHT is a dark dance party in New Haven, CT, taking place the 2nd Saturday of every month. A welcoming place for the Goths, Rivetheads, Baby Bats, Cyberpunks, Thrashers, Fetish Queens, Pastels, Elders, the Dark-Curious, and Alternative types.">
+		<meta name="description" content="NIGHT is a goth night/dark dance party in New Haven, CT, taking place the 2nd Saturday of every month at Partners Cafe. A welcoming place for the Goths, Rivetheads, Baby Bats, Cyberpunks, Thrashers, Fetish Queens, Pastels, Elders, the Dark-Curious, and Alternative types.">
 		<meta name="keywords" content="night, new haven, night new haven, new haven goth night, connecticut goth, connecticut goth night, night partners, partners new haven, nhv, connecticut, ct, goth, club, goth club, goth night, dark dance party, dance party, club night, post-punk, darkwave, industrial, rivethead, ebm, new wave, witch house, partners, fetish, alternative, lgbtq, queer friendly, gay-friendly, trans-friendly">
 		<meta name="author" content="John Mordecai">
 		
 		<meta property="og:title" content="NIGHT" />
 		<meta property="og:site_name" content="NIGHT" />
-		<meta property="og:description" content="NIGHT is a dark dance party in New Haven, CT, taking place on the 2nd Saturday of each month." />
+		<meta property="og:description" content="NIGHT is a goth night/dark dance party in New Haven, CT, taking place on the 2nd Saturday of each month at Partners Cafe." />
 		<meta property="og:image" content="https://nightnewhaven.club/img/night-og.jpg" />
 		<meta property="og:image:type" content="image/jpg" />
 		<meta property="og:url" content="https://nightnewhaven.club" />
 				
 		<!-- Favicon -->
-		<link rel="icon" type="image/png" href="<?= IMG_PATH; ?>/favicon_blk.png" />
+		<link rel="icon" type="image/png" href="https://nightnewhaven.club/img/favicon-wht-192.png" />
 		
 		<!-- Bootstrap CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
